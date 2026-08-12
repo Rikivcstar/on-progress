85fb97eb6545d8b5174f4f65f9dc258993194bdf
Rikivcstar <rikireza3r@gmail.com>
2026-08-13 02:32:00 +0700

    updated day 2 sebentaran saja

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProsesCheckoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+
+Route::post('/chechout', ProsesCheckoutController::class);
