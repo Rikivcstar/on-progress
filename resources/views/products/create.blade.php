c430c6ba84b94c2ed3b5af025d606d1a900f9a96
Rikivcstar <rikireza3r@gmail.com>
2026-08-23 23:44:13 +0700

    update progrees belajar day 3

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -3,6 +3,9 @@
     <form method="POST" action="{{ route('products.store') }}">
         @csrf
         <input type="text" name="name" placeholder="Masukan Nama"><br>
+        @error('name')
+            <p style="color: red; font-style: italic">{{ $message }}</p>
+        @enderror
         <input type="number" name="price" placeholder="Masukan Harga"><br>
         <input type="number" name="stock" placeholder="Masukan Stock"><br>
         <button type="submit">Simpan</button>
