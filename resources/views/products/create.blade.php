a7c5e41d0a1cfe96c39832274ee153e298047ae4
Rikivcstar <rikireza3r@gmail.com>
2026-08-29 09:30:36 +0700

    day 6 belajar middleware

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -7,7 +7,13 @@
             <p style="color: red; font-style: italic">{{ $message }}</p>
         @enderror
         <input type="number" name="price" placeholder="Masukan Harga"><br>
+         @error('price')
+            <p style="color: red; font-style: italic">{{ $message }}</p>
+        @enderror
         <input type="number" name="stock" placeholder="Masukan Stock"><br>
+         @error('stock')
+            <p style="color: red; font-style: italic">{{ $message }}</p>
+        @enderror
         <button type="submit">Simpan</button>
     </form>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,29 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::middleware('auth')->group(function() {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/products', ProductController::class);
+});
+
+require __DIR__.'/auth.php';
+
 
 // Route::get('/products/{id}', function($id){
 //     return Product::findOrFail($id);
@@ -17,5 +34,3 @@ Route::get('/', function () {
 //         return 'Halaman Untuk Admin Saja Kelola Product';
 //     })->name('products.index');
 // });
-
-Route::resource('/products', ProductController::class);
