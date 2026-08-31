e0ee009f18651367a31e26688f9b8b1150cc9984
Rikivcstar <rikireza3r@gmail.com>
2026-08-31 10:07:12 +0700

    day 7

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,10 +4,15 @@
     @if(session('success'))
         <p style="color:green">{{ session('success') }}</p>
     @endif
-    <h2>Daftar Product</h2>
+    <h2>Daftar Product toko electronic</h2>
+    <p>Total Products :{{ $products->count() }}</p>
     @foreach ($products as $product)
         <ul>
-            <li>Nama Product : {{ $product->name }} - Harga Rp.{{ number_format($product->price) }}</li>
+            <li>Nama Product : {{ $product->name }} - Harga Rp.{{ number_format($product->price) }}
+                @if($product->stock < 10)
+                    <strong style="color:red">(Stok Menipis!)</strong>
+                @endif
+            </li>
         </ul>
     @endforeach
 </div>
