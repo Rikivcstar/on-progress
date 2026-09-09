d6f6b8c0cfbd8cf5083a3b5d0c23ef96403a3ca0
Rikivcstar <rikireza3r@gmail.com>
2026-09-10 00:42:57 +0700

    learn day 10

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,13 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Tag extends Model
 {
-    //
-    protected $fillable = ['name'];
+    protected $guarded = ['id'];
 
     public function products()
     {
-        return $this->hasMany(Product::class);
+        return $this->belongsToMany(Product::class);
     }
 }
