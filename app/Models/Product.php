b887de2a0e5a5b44b1a0024a6149a81eda17ef45
Rikivcstar <rikireza3r@gmail.com>
2026-09-14 01:31:19 +0700

    belajar day 12

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,9 +5,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
+
+    use SoftDeletes;
     protected $guarded = ['id'];
 
     public function category()
